0ecdba4fc4ff438f0da5b1dca7acd4a2a62ac523
yonit garg <yonit.garg126694@marwadiuniversity.ac.in>
2026-04-08 23:08:19 +0530

    fix authRoutes handler error

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const { register, login } = require("../controllers/authController");
+const authController = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 
-module.exports = router;
+module.exports = {
+  register,
+  login
+};
