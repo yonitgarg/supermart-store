f3847c4ea9ff306cda923bf4597ffe0be81ec32e
yonit garg <yonit.garg126694@marwadiuniversity.ac.in>
2026-04-15 09:16:08 +0530

    update

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
   category: String,
-  stock: Number
+  stock: Number,
+  image: String
 });
 
 module.exports = mongoose.model("Product", productSchema);
